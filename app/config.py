8ab83af007d8befd7c6c735254a40f177113e489
Dark Coder <codewithdark90@gmail.com>
2025-01-22 12:55:24 +0500

    Update config.py

--- app/config.py
+++ app/config.py
@@ -1,6 +1,10 @@
 from pydantic_settings import BaseSettings
 from typing import List
 from functools import lru_cache
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
 
 class Settings(BaseSettings):
     PROJECT_NAME: str = "Healthcare AI API"
@@ -12,17 +16,15 @@ class Settings(BaseSettings):
     # Model Paths
     MODEL_PATH: str = "models"
     EYE_MODEL_PATH: str = f"{MODEL_PATH}/eye_model.pth"
-    PNEUMONIA_MODEL_PATH: str = f"{MODEL_PATH}/"
-    SKIN_MODEL_PATH: str = f"{MODEL_PATH}/skin_disease_model"
+    PNEUMONIA_MODEL_PATH: str = f"{MODEL_PATH}/pneumonia_model.pth"
+    SKIN_MODEL_PATH: str = f"{MODEL_PATH}/skin_cancer_model.pth"
     HEART_MODEL_PATH: str = f"{MODEL_PATH}/heart_disease_model"
     BRAIN_MODEL_PATH: str = f"{MODEL_PATH}/brain_tumor_model.pth"
     
     # LLM Configuration
-    GROQ_API_KEY: str = ""
-    GROQ_MODEL_NAME: str = "mixtral-8x7b-32768"  # or your preferred Groq model
-    
-    # OpenAI Configuration (backup/alternative)
-    OPENAI_API_KEY: str = ""
+    GROQ_API_KEY: str = os.getenv("GROQ_API_KEY")
+    GROQ_MODEL_NAME: str = os.getenv("GROQ_MODEL_NAME")
+
     
     class Config:
         env_file = ".env"
